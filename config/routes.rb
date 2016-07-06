7779d4cad54a49c2a838973714818136178906dc
Matthew Steinwachs <matt.steinwachs@gmail.com>
2016-07-06 14:20:41 -0700

    Added a page instantiating SeeIt with no options provided under the route /no_data

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root to: "application#index"
 
   get "init" => "application#init"
+  get "no_data" => "application#no_data"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
